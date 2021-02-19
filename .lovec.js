90ff75e8e5a037f205affaecb0ce836a0ca8e88c
chauhansaahil <65437646+chauhansaahil@users.noreply.github.com>
2021-02-19 20:23:20 +0530

    Create .lovec.js

---
+++ .lovec.js
@@ -0,0 +1,34 @@
+const Asena = require ('../ events');
+const {MessageType} = require ('@ adiwajshing / baileys');
+
+
+Asena.addCommand ({pattern: 'lovec', fromMe: true}, (async (message, match) => {
+
+
+    await message.sendMessage('*I*');
+    await message.sendMessage('*I L*');
+    await message.sendMessage('*I LO*');
+    await message.sendMessage('*I LOV*');
+    await message.sendMessage('*I LOVE*');
+    await message.sendMessage('*I LOVE Y*');
+    await message.sendMessage('*I LOVE YO*');
+    await message.sendMessage('*I LOVE YOU 😍*');
+    await message.sendMessage('*I LOVE YOU S*');
+    await message.sendMessage('*I LOVE YOU SO*');
+    await message.sendMessage('*I LOVE YOU SO M*');
+    await message.sendMessage('*I LOVE YOU SO MU*');
+    await message.sendMessage('*I LOVE YOU SO MUC*');
+    await message.sendMessage('*I LOVE YOU SO MUCH 💞*');
+    await message.sendMessage('*I LOVE YOU SO MUCH CHUDAIL💞 *');
+
+
+}));
+
+
+Asena.addCommand({pattern: 'lovec', fromMe: true}, (async (message, match) => {
+
+
+    await message.sendMessage("*Codded by @chauhansaahil*\n*To say best way ı love you chudail.*\n💻Usage: *.lovec*")
+
+
+}));
